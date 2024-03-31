927591978f50cebe6d7ef9699c23c33320b1ec12
GMilbusiak <gabriel.milbusiak@gmail.com>
2024-03-31 19:36:43 -0300

    Criação projeto mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
